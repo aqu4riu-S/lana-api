41d79e80fd9aa128a87050d42901769c2434e2a8
Bruno Campos <bmvc98@gmail.com>
2024-03-20 00:05:42 +0000

    scraping fixes

--- main.py
+++ main.py
@@ -20,88 +20,109 @@ extension = "/critic-reviews"
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
 
+albums_objs_lst = []
+reviews_objs_lst = []
+sources_lst = []
 albums_lst = []
-reviews_lst = []
 review_count = 0
+
+
+def add_source(src):
+    if src not in sources_lst:
+        sources_lst.append(src)
+
+def add_album(alb):
+    if alb not in albums_lst:
+        albums_lst.append(alb)
+
+def create_authors_lst():
+    authors_lst = []
+    for index, src in enumerate(sources_lst):
+        src_obj = {
+            "id": str(index+1),
+            "name": src
+        }
+        authors_lst.append(src_obj)
+    return authors_lst
+    
+
+def create_file_json(file_name, obj):
+    with open(file_name+".json", "w") as file:
+        file.write(json.dumps(obj, indent=4))
+        file.close()
+        
 
 for idx, url in enumerate(urls):
     
     response = requests.get(url, headers={"User-Agent": user_agent})
-    
-    #print(response.status_code)
+
+    album = ""
+
     
     if response.status_code == 200:
 
         soup = BeautifulSoup(response.text, 'html.parser')
-        #print(soup)
+
         artist = soup.find(class_="band_name").text
-        
         div = soup.find(class_="product_title")
         album = div.find('h1').text
         span = soup.find(class_="data")
         publisher = span.find('span').text.strip()
         score = soup.select('div.metascore_w.album')[0].text.strip()
-        print(score)
 
-        #print(artist, album, publisher)
+        # add album to list
+        add_album(album)
         
+        # create album object and append it to list of album objects
         album_obj = {
-            "id": idx+1,
+            "id": str(idx+1),
             "title": album,
             "artist": artist,
             "publisher": publisher,
             "score": score
         }
         
-        albums_lst.append(album_obj)
+        albums_objs_lst.append(album_obj)
         
         
     
     response2 = requests.get(url+extension, headers={"User-Agent": user_agent})
-    #print(response2.status_code)
     
     if response2.status_code == 200:
         
         soup = BeautifulSoup(response2.text, 'html.parser')
         
         ol = soup.find(class_="critic_reviews")
-        
         divs = ol.find_all(class_="review_content")
-        #print(divs)
+
         for index, div in enumerate(divs):
             source = div.find(class_="source").text.strip()
             score = div.find(class_="review_grade").text.strip()
             review = div.find(class_="review_body").text.strip()
-            print("Source: {}\nScore: {}\nReview: {}".format(source, score, review))
-        
+           
+            # add source to list
+            add_source(source)
+            
+            # create review object and append it to list of review objects
             review_obj = {
-                "id": review_count+1,
+                "id": str(review_count+1),
                 "source": source,
                 "rating": score,
-                "content": review
+                "content": review,
+                "author_id": str(sources_lst.index(source) + 1),
+                "album_id": str(albums_lst.index(album) + 1)
             }
+        
+            reviews_objs_lst.append(review_obj)  
             
+            # update counter to keep track of the number of reviews
             review_count += 1
-        
-            reviews_lst.append(review_obj)  
     
-    #print(reviews_lst)  
 
+create_file_json("albums", albums_objs_lst)       
+create_file_json("reviews", reviews_objs_lst)       
+create_file_json("authors", create_authors_lst())       
         
-album_json = json.dumps(albums_lst, indent=4)
-        
-with open("albums.json", "w") as file:
-    file.write(album_json) 
-    file.close()     
-    
-    
-reviews_json = json.dumps(reviews_lst, indent=4)
-    
-with open("reviews.json", "w") as file:
-    file.write(reviews_json)
-    file.close()  
-        
-    
-    
+
 # get year of release for the albums
 # fix the encoding problem
